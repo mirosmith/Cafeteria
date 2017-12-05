1eeb20f91827474b7476966aefc0cfe37efc4e0d
mirosmith <simir@seznam.cz>
2017-12-05 12:20:57 +0100

    added configuration files for MySQL db and H2 db

---
+++ Cafeteria/src/test/java/mainBootApp/CategoryRepositoryTestIT.java
@@ -0,0 +1,35 @@
+package mainBootApp;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import mainBootApp.model.Category;
+import mainBootApp.repositories.CategoryRepository;
+
+
+/**
+ * 
+ * Integration test
+ *
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CategoryRepositoryTestIT {
+	
+	@Autowired
+	CategoryRepository catRepo;
+
+	@Test
+	public void findByNameTest() {
+		
+		Category category = catRepo.findByName("American");
+		
+		assertEquals("American", category.getName());
+		
+	}
+}
--- Cafeteria/src/test/java/mainBootApp/MyControllerTest.java
+++ Cafeteria/src/test/java/mainBootApp/MyControllerTest.java
@@ -3,6 +3,9 @@ package mainBootApp;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,6 +14,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import mainBootApp.cotrollers.MyController;
@@ -38,6 +44,17 @@ public class MyControllerTest {
 		MockitoAnnotations.initMocks(this);
 		
 		controller = new MyController(service);		
+	}
+	
+	@Test
+	public void showCoffeesMvcTest() throws Exception {
+		
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		
+		mockMvc.perform(get("/"))
+			   .andExpect(status().isOk())
+			   .andExpect(view().name("coffees"))
+			   .andExpect(MockMvcResultMatchers.model().attributeExists("coffeeList"));
 	}
 	
 	@Test
